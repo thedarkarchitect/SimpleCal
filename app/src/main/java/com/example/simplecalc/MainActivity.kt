c0e6c13d9ede1df91821fb75951b4779948a8244
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-31 13:26:29 +0300

    fourth commit

--- app/src/main/java/com/example/simplecalc/MainActivity.kt
+++ app/src/main/java/com/example/simplecalc/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var tvInput: TextView? = null
+    // to check the last input
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +22,37 @@ class MainActivity : AppCompatActivity() {
         //View returns the button clicked
 //        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
         tvInput?.append((view as Button).text)//beacuse view is a button we need to convert it and then get the text
+        lastNumeric = true
+        lastDot = false
     }
 
     fun onClear(view: View){
         tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")//this adds the dot to the input
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        tvInput?.text?.let{
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+            }
+        }
+    }
+    private fun isOperatorAdded(value : String) : Boolean {
+        return if(value.startsWith("-")){//this will deal with negative entries by ignoring them
+            false
+        }else{
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
     }
 }
