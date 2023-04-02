f4f774af71cfbf49ff3d07114fb6f380fd0de6ce
thedarkarchitect <romanreynolds240@gmail.com>
2023-04-02 10:00:15 +0300

    final commit

--- app/src/main/java/com/example/simplecalc/MainActivity.kt
+++ app/src/main/java/com/example/simplecalc/MainActivity.kt
@@ -45,6 +45,89 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    fun onEqual(view: View){
+
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try{
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)//this returns everything after the index entered
+
+                }
+
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+
+                    //separate the string values entered
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+
+                }else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+
+                    //separate the string values entered
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+
+                }else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+
+                    //separate the string values entered
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+
+                    //separate the string values entered
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+
+                }
+
+            }catch (e : java.lang.ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result : String): String{
+        var value = result
+
+        if(result.contains(".0")){
+            value = result.substring(0, result.length-2)//helps element the float after point
+        }
+        return value
+    }
+
     private fun isOperatorAdded(value : String) : Boolean {
         return if(value.startsWith("-")){//this will deal with negative entries by ignoring them
             false
